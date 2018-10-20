6bb2477719ba6d5406a7d907ae3488be092d6c3a
Arujingle <aruzhanzhyngylbay@gmail.com>
2018-10-20 11:54:36 +0600

    4 test

--- tests/fibonacci_test.py
+++ tests/fibonacci_test.py
@@ -5,4 +5,18 @@ from fibonacci import Fibonacci
 fib = Fibonacci()
 
 class FibonacciTest(TestCase):
-	pass
+	def test_recursive_implementation_with_one_argument(self):
+		result = fib.recursive_implementation(34)
+		self.assertEqual(result, 5702887)
+
+	def test_dynamic_implementation_with_one_argument(self):
+		result = fib.dynamic_implementation(34)
+		self.assertEqual(result, 5702887)
+
+	def test_recursive_implementation_zero(self):
+		result = fib.recursive_implementation(0)
+		self.assertEqual(result, 0)
+
+	def test_dynamic_implementation_zero(self):
+		result = fib.dynamic_implementation(0)
+		self.assertEqual(result, 0)
